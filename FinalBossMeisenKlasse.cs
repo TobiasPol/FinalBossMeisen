73f7810d8d3ef9a33838be793cd3fbd233b5767c
Tobi <Tobi@WINDOWS-M4129B3>
2023-10-09 23:19:41 +0200

    Add project files.

---
+++ FinalBossMeisenKlasse.cs
@@ -0,0 +1,215 @@
+using AntMe.Deutsch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntMe.Player.FinalBossMeisen
+{
+    /// <summary>
+    /// Diese Datei enthält die Beschreibung für deine Ameise. Die einzelnen Code-Blöcke 
+    /// (Beginnend mit "public override void") fassen zusammen, wie deine Ameise in den 
+    /// entsprechenden Situationen reagieren soll. Welche Befehle du hier verwenden kannst, 
+    /// findest du auf der Befehlsübersicht im Wiki.
+    /// 
+    /// Wenn du etwas Unterstützung bei der Erstellung einer Ameise brauchst, findest du
+    /// in den AntMe!-Lektionen ein paar Schritt-für-Schritt Anleitungen.
+    ///
+    /// Link zum Wiki: https://wiki.antme.net
+    /// </summary>
+    [Spieler(
+        Volkname = "FinalBossMeisen",   // Hier kannst du den Namen des Volkes festlegen
+        Vorname = "",       // An dieser Stelle kannst du dich als Schöpfer der Ameise eintragen
+        Nachname = ""       // An dieser Stelle kannst du dich als Schöpfer der Ameise eintragen
+    )]
+
+    /// Kasten stellen "Berufsgruppen" innerhalb deines Ameisenvolkes dar. Du kannst hier mit
+    /// den Fähigkeiten einzelner Ameisen arbeiten. Wie genau das funktioniert kannst du der 
+    /// Lektion zur Spezialisierung von Ameisen entnehmen.
+    [Kaste(
+        Name = "Standard",                  // Name der Berufsgruppe
+        AngriffModifikator = 0,             // Angriffsstärke einer Ameise
+        DrehgeschwindigkeitModifikator = 0, // Drehgeschwindigkeit einer Ameise
+        EnergieModifikator = 0,             // Lebensenergie einer Ameise
+        GeschwindigkeitModifikator = 0,     // Laufgeschwindigkeit einer Ameise
+        LastModifikator = 0,                // Tragkraft einer Ameise
+        ReichweiteModifikator = 0,          // Ausdauer einer Ameise
+        SichtweiteModifikator = 0           // Sichtweite einer Ameise
+    )]
+    public class FinalBossMeisenKlasse : Basisameise
+    {
+        #region Kasten
+
+        /// <summary>
+        /// Jedes mal, wenn eine neue Ameise geboren wird, muss ihre Berufsgruppe
+        /// bestimmt werden. Das kannst du mit Hilfe dieses Rückgabewertes dieser 
+        /// Methode steuern.
+        /// </summary>
+        /// <param name="anzahl">Anzahl Ameisen pro Kaste</param>
+        /// <returns>Name der Kaste zu der die geborene Ameise gehören soll</returns>
+        public override string BestimmeKaste(Dictionary<string, int> anzahl)
+        {
+            // Gibt den Namen der betroffenen Kaste zurück.
+            return "Standard";
+        }
+
+        #endregion
+
+        #region Fortbewegung
+
+        /// <summary>
+        /// Wenn die Ameise keinerlei Aufträge hat, wartet sie auf neue Aufgaben. Um dir das 
+        /// mitzuteilen, wird diese Methode hier aufgerufen.
+        /// </summary>
+        public override void Wartet()
+        {
+        }
+
+        /// <summary>
+        /// Erreicht eine Ameise ein drittel ihrer Laufreichweite, wird diese Methode aufgerufen.
+        /// </summary>
+        public override void WirdMüde()
+        {
+        }
+
+        /// <summary>
+        /// Wenn eine Ameise stirbt, wird diese Methode aufgerufen. Man erfährt dadurch, wie 
+        /// die Ameise gestorben ist. Die Ameise kann zu diesem Zeitpunkt aber keinerlei Aktion 
+        /// mehr ausführen.
+        /// </summary>
+        /// <param name="todesart">Art des Todes</param>
+        public override void IstGestorben(Todesart todesart)
+        {
+        }
+
+        /// <summary>
+        /// Diese Methode wird in jeder Simulationsrunde aufgerufen - ungeachtet von zusätzlichen 
+        /// Bedingungen. Dies eignet sich für Aktionen, die unter Bedingungen ausgeführt werden 
+        /// sollen, die von den anderen Methoden nicht behandelt werden.
+        /// </summary>
+        public override void Tick()
+        {
+        }
+
+        #endregion
+
+        #region Nahrung
+
+        /// <summary>
+        /// Sobald eine Ameise innerhalb ihres Sichtradius einen Apfel erspäht wird 
+        /// diese Methode aufgerufen. Als Parameter kommt das betroffene Stück Obst.
+        /// </summary>
+        /// <param name="obst">Das gesichtete Stück Obst</param>
+        public override void Sieht(Obst obst)
+        {
+        }
+
+        /// <summary>
+        /// Sobald eine Ameise innerhalb ihres Sichtradius einen Zuckerhügel erspäht wird 
+        /// diese Methode aufgerufen. Als Parameter kommt der betroffene Zuckerghügel.
+        /// </summary>
+        /// <param name="zucker">Der gesichtete Zuckerhügel</param>
+        public override void Sieht(Zucker zucker)
+        {
+        }
+
+        /// <summary>
+        /// Hat die Ameise ein Stück Obst als Ziel festgelegt, wird diese Methode aufgerufen, 
+        /// sobald die Ameise ihr Ziel erreicht hat. Ab jetzt ist die Ameise nahe genug um mit 
+        /// dem Ziel zu interagieren.
+        /// </summary>
+        /// <param name="obst">Das erreichte Stück Obst</param>
+        public override void ZielErreicht(Obst obst)
+        {
+        }
+
+        /// <summary>
+        /// Hat die Ameise eine Zuckerhügel als Ziel festgelegt, wird diese Methode aufgerufen, 
+        /// sobald die Ameise ihr Ziel erreicht hat. Ab jetzt ist die Ameise nahe genug um mit 
+        /// dem Ziel zu interagieren.
+        /// </summary>
+        /// <param name="zucker">Der erreichte Zuckerhügel</param>
+        public override void ZielErreicht(Zucker zucker)
+        {
+        }
+
+        #endregion
+
+        #region Kommunikation
+
+        /// <summary>
+        /// Markierungen, die von anderen Ameisen platziert werden, können von befreundeten Ameisen 
+        /// gewittert werden. Diese Methode wird aufgerufen, wenn eine Ameise zum ersten Mal eine 
+        /// befreundete Markierung riecht.
+        /// </summary>
+        /// <param name="markierung">Die gerochene Markierung</param>
+        public override void RiechtFreund(Markierung markierung)
+        {
+        }
+
+        /// <summary>
+        /// So wie Ameisen unterschiedliche Nahrungsmittel erspähen können, entdecken Sie auch 
+        /// andere Spielelemente. Entdeckt die Ameise eine Ameise aus dem eigenen Volk, so 
+        /// wird diese Methode aufgerufen.
+        /// </summary>
+        /// <param name="ameise">Erspähte befreundete Ameise</param>
+        public override void SiehtFreund(Ameise ameise)
+        {
+        }
+
+        /// <summary>
+        /// So wie Ameisen unterschiedliche Nahrungsmittel erspähen können, entdecken Sie auch 
+        /// andere Spielelemente. Entdeckt die Ameise eine Ameise aus einem befreundeten Volk 
+        /// (Völker im selben Team), so wird diese Methode aufgerufen.
+        /// </summary>
+        /// <param name="ameise">Erspähte verbündete Ameise</param>
+        public override void SiehtVerbündeten(Ameise ameise)
+        {
+        }
+
+        #endregion
+
+        #region Kampf
+
+        /// <summary>
+        /// So wie Ameisen unterschiedliche Nahrungsmittel erspähen können, entdecken Sie auch 
+        /// andere Spielelemente. Entdeckt die Ameise eine Ameise aus einem feindlichen Volk, 
+        /// so wird diese Methode aufgerufen.
+        /// </summary>
+        /// <param name="ameise">Erspähte feindliche Ameise</param>
+        public override void SiehtFeind(Ameise ameise)
+        {
+        }
+
+        /// <summary>
+        /// So wie Ameisen unterschiedliche Nahrungsmittel erspähen können, entdecken Sie auch 
+        /// andere Spielelemente. Entdeckt die Ameise eine Wanze, so wird diese Methode aufgerufen.
+        /// </summary>
+        /// <param name="wanze">Erspähte Wanze</param>
+        public override void SiehtFeind(Wanze wanze)
+        {
+        }
+
+        /// <summary>
+        /// Es kann vorkommen, dass feindliche Lebewesen eine Ameise aktiv angreifen. Sollte 
+        /// eine feindliche Ameise angreifen, wird diese Methode hier aufgerufen und die 
+        /// Ameise kann entscheiden, wie sie darauf reagieren möchte.
+        /// </summary>
+        /// <param name="ameise">Angreifende Ameise</param>
+        public override void WirdAngegriffen(Ameise ameise)
+        {
+        }
+
+        /// <summary>
+        /// Es kann vorkommen, dass feindliche Lebewesen eine Ameise aktiv angreifen. Sollte 
+        /// eine Wanze angreifen, wird diese Methode hier aufgerufen und die Ameise kann 
+        /// entscheiden, wie sie darauf reagieren möchte.
+        /// </summary>
+        /// <param name="wanze">Angreifende Wanze</param>
+        public override void WirdAngegriffen(Wanze wanze)
+        {
+        }
+
+        #endregion
+    }
+}
